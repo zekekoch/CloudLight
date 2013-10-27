f39a5b197ea1aecbf67556e26a5986682c809df2
Zeke Koch <zekekoch@Selinas-MacBook-Air.local>
2013-10-27 13:01:08 -0700

    added some HSV stuff.
    still very much a work in progress

--- CloudLight.ino
+++ CloudLight.ino
@@ -9,7 +9,8 @@
 //   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
 //   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
 //   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
+const byte ledCount = 88;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(ledCount, PIN, NEO_GRB + NEO_KHZ800);
 
 void setup() {
   strip.begin();
@@ -18,7 +19,56 @@ void setup() {
   pinMode(1, INPUT);
 }
 
-void loop() {
+void loopb()
+{
+  setAllColor(0, 0, 255);
+  delay(random(500,5000));
+  for(int i = 0;i<128;i++)
+    flicker(240, 128);
+  colorWipe(strip.Color(0, 0, 255), 1); // Blue
+}
+
+void loop()
+{
+  sunset(0);
+  sunset(15);
+  sunset(30);
+  sunset(45);
+  sunset(45);
+}
+
+void sunset(int  hue)
+{
+  const byte delayTime = 20;
+  for(int i = 0;i<=255;i++)
+  {
+    setAllHSV(hue, i, 128);
+    delay(delayTime);
+  }
+  for(int i = 128;i>=0;i--)
+  {
+    setAllHSV(hue, 255, i);
+    delay(delayTime);
+  }
+}
+
+int sunsetHue(int myHue)
+{
+  myHue--;
+  if (myHue == 0)
+  {
+    myHue = 360;
+  }
+  
+  if (myHue = 255)
+  {
+    myHue = 45;
+  }
+  return myHue;
+}
+
+
+void looped() {
   static boolean mode = false;
   static boolean modeLightning = false;
   // Some example procedures showing how to display to the pixels:
@@ -47,6 +97,85 @@ void loop() {
 //  colorWipe(strip.Color(0, 255, 0), 50); // Green
 //  rainbow(20);
 //  rainbowCycle(20);
+}
+
+void setAllColor(byte r, byte g, byte b)
+{
+  for (int i = 0; i< ledCount;i++)
+  {
+    strip.setPixelColor(i, r, g, b);
+  }
+}
+
+void setAllHSV(unsigned int  h, byte  s, byte  v)
+{
+  for (int i = 0; i< ledCount;i++)
+  {
+    setHSV(i, h, s, v);
+  }
+  strip.show();
+}
+
+void flicker(int thishue, int thissat) {            //-m9-FLICKER EFFECT
+  int random_bright = random(0,255);
+  int random_delay = random(10,100);
+  int random_bool = random(0,random_bright);
+  if (random_bool < 10) {
+    for(int i = 0 ; i < ledCount; i++ ) {
+      setHSV(i, thishue, thissat, random_bright); 
+    }
+    strip.show();    
+    delay(random_delay);
+  }
+}
+
+void setHSV(int led, unsigned int hue, byte sat, byte val)
+{
+        unsigned char r,g,b;
+        unsigned int H_accent = hue/60;
+        unsigned int bottom = ((255 - sat) * val)>>8;
+        unsigned int top = val;
+        unsigned char rising  = ((top-bottom)  *(hue%60   )  )  /  60  +  bottom;
+        unsigned char falling = ((top-bottom)  *(60-hue%60)  )  /  60  +  bottom;
+
+        switch(H_accent) {
+        case 0:
+                r = top;
+                g = rising;
+                b = bottom;
+                break;
+
+        case 1:
+                r = falling;
+                g = top;
+                b = bottom;
+                break;
+
+        case 2:
+                r = bottom;
+                g = top;
+                b = rising;
+                break;
+
+        case 3:
+                r = bottom;
+                g = falling;
+                b = top;
+                break;
+
+        case 4:
+                r = rising;
+                g = bottom;
+                b = top;
+                break;
+
+        case 5:
+                r = top;
+                g = bottom;
+                b = falling;
+                break;
+        }
+        strip.setPixelColor(led, r, g, b);
 }
 
 // Fill the dots one after the other with a color
